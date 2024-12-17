c98aaec378db72edad3b4b65535308b5275fc45e
hand3l <handel.manobanda@epn.edu.ec>
2024-12-17 08:42:10 -0500

    First Commit

---
+++ page2/page.js
@@ -0,0 +1,17 @@
+alert("Bienvenido a la esfot")
+
+
+var val1=2;
+var val2=6;
+var suma;
+
+suma=val1+val2
+
+alert("LA SUMA ES " + suma);
+
+let valor1=50;
+let valor2=25;
+let suma1;
+suma1=valor1+valor2
+
+alert("LA SUMA ES " +suma1);
